6697fed9be2c94265ca6640d69ebafef000bab69
taehee <rnxogml3000@naver.com>
2022-07-22 18:00:04 +0900

    2022.07.22

--- java/SW Expert Academy/src/swea/d1/D1_2071.java
+++ java/SW Expert Academy/src/swea/d1/D1_2071.java
@@ -1,18 +1,22 @@
 package swea.d1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 // 평균값 구하기
 // https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QRnJqA5cDFAUq&categoryId=AV5QRnJqA5cDFAUq&categoryType=CODE&problemTitle=2071&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1&&&&&&&&&
 public class D1_2071 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= T; t++) {
-			System.out.print("#" + t + " ");
+			bw.write("#" + t + " ");
+			bw.flush();
 
 			int sum = 0;
 			String[] temp = br.readLine().split(" ");
@@ -20,7 +24,8 @@ public class D1_2071 {
 				sum += Integer.parseInt(temp[i]);
 			}
 			int avg = (int) Math.round(sum / 10.0);
-			System.out.println(avg);
+			bw.write(avg + "\n");
+			bw.flush();
 		}
 
 	}
--- java/SW Expert Academy/src/swea/d2/D2_1970.java
+++ java/SW Expert Academy/src/swea/d2/D2_1970.java
@@ -1,8 +1,9 @@
 package swea.d2;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
 
 // 쉬운 거스름돈
 // https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PsIl6AXIDFAUq&categoryId=AV5PsIl6AXIDFAUq&categoryType=CODE&problemTitle=1970&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
@@ -13,24 +14,28 @@ public class D2_1970 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= T; t++) {
 			int n = Integer.parseInt(br.readLine());
-			System.out.println("#" + t);
+			bw.write("#" + t + "\n");
+			bw.flush();
 
 			for (int i = 0; i < 8; i++) {
 				int cnt = 0;
 				int temp = n / change[i];
-				
+
 				if (temp >= 1) {
 					cnt = temp;
 					n -= change[i] * temp;
 				}
-				System.out.print(cnt + " ");
+				bw.write(cnt + " ");
+				bw.flush();
 			}
-			System.out.println();
+			bw.newLine(); // 줄바꿈
+//			bw.write("\n");
 		}
 	}
 }
