10b60ee0c95ac3bc9c87aa52b94e3ae9909dbfa3
taehee <rnxogml3000@naver.com>
2022-07-26 17:50:06 +0900

    2022.07.23

---
+++ java/Backjoon/src/com/backjoon/silver/silver1/Main_1931.java
@@ -0,0 +1,26 @@
+package com.backjoon.silver.silver1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_1931 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+
+		int[][] arr = new int[n][2];
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+	}
+}
