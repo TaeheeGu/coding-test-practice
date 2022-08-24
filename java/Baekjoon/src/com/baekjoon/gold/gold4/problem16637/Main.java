575ab4c4c092edac27e86ea3a48aac44e09b5019
good-da22 <rnxogml3000@gmail.com>
2022-08-24 17:48:58 +0900

    2022.08.24

--- java/Baekjoon/src/com/baekjoon/gold/gold4/problem16637/Main.java
+++ java/Baekjoon/src/com/baekjoon/gold/gold4/problem16637/Main.java
@@ -5,11 +5,19 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 
+
+A형 기출 복슾 필
 // 백준, 16637번, 괄호 추가하기, 골드4
 // https://www.acmicpc.net/problem/16637
 public class Main {
-	
+
+	private static int N;
+	private static List<Character> ops;
+	private static List<Integer> nums;
+
 	static int max;
 
 	public static void main(String[] args) throws IOException {
@@ -21,41 +29,64 @@ public class Main {
 
 		StringBuilder sb = new StringBuilder();
 
-		int N = Integer.parseInt(in.readLine());
-		
-		char[] expr = in.readLine().toCharArray();
-		
+		N = Integer.parseInt(in.readLine());
+
+		char[] c = in.readLine().toCharArray();
+
+		nums = new ArrayList<>();
+		ops = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			if (i % 2 == 0) {
+				nums.add(c[i] - '0');
+			} else {
+				ops.add(c[i]);
+			}
+		}
+
 		max = Integer.MIN_VALUE;
-		
-		calc(expr);
-		
+
+		dfs(0, nums.get(0));
+
 		sb.append(max);
 		out.write(sb.toString());
 		out.close();
 	}
-	
-	private static void calc(char[] expr) {
-		
-		int num1 = expr[0] - '0';
-		for (int i = 1; i < expr.length; i+=2) {
-			
-			int num2 = expr[i + 1] - '0';		
-			char temp = expr[i];
 
-			switch (temp) {
-			case '+':
-				num1 = num1 + num2;
-				break;
-			case '-':
-				num1 = num1 - num2;
-				break;
-			case '*':
-				num1 = num1 * num2;
-				break;
-			}	
+	private static void dfs(int idx, int sum) {
+		if (idx >= ops.size()) {
+			max = Math.max(max, sum);
+			return;
 		}
-		max = Math.max(max, num1);
-		
-		return;
+
+		// 괄호 안치는 경우
+		// 지금까지의 연산 결과에 연산자 오른쪽 숫자를 연산한다.
+		dfs(idx + 1, calc(sum, nums.get(idx + 1), ops.get(idx)));
+
+		// 괄호 치는 경우
+		// 다음으로 오는 연산자를 먼저 계산 후 왼쪽 값과 연산을 실행한다.
+		if (idx < ops.size() - 1) {
+			int temp = calc(nums.get(idx + 1), nums.get(idx + 2), ops.get(idx + 1));
+			
+			dfs(idx + 2,  calc(sum, temp, ops.get(idx)));
+
+		}
+
 	}
+
+	private static int calc(int num1, int num2, char operator) {
+
+		switch (operator) {
+		case '+':
+			return num1 + num2;
+		case '-':
+			return num1 - num2;
+
+		case '*':
+			return num1 * num2;
+		}
+
+		return 0;
+	}
+
 }
---
+++ java/SW Expert Academy/src/com/swea/advanced/problem1/Solution.java
@@ -0,0 +1,26 @@
+package com.swea.advanced.problem1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// No. 1 [연습문제] 낚시터 자리잡기
+// https://swexpertacademy.com/main/talk/codeBattle/problemDetail.do?contestProbId=AYLNTUm6AswDFASv&categoryId=AYLNSmY6An0DFASv&categoryType=BATTLE&battleMainPageIndex=
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+
+		System.setIn(new FileInputStream("input.txt"));
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringBuilder sb = new StringBuilder();
+
+		out.write(sb.toString());
+		out.close();
+	}
+}
